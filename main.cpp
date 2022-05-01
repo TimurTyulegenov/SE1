bc609a2504cb59c1f92abb1046a149346531c3f1
TyulegenovValeev <timatim2001@mail.ru>
2022-05-02 00:14:46 +0500

    main.cpp: add 2 number in program

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b;
+    cout<<"Введите два целых числа"<<endl;
+    cout<<"Число А:";
+    cin>>a;
+    cout<<"Число B:";
+    cin>>b;
+    return 0;
+}
 
